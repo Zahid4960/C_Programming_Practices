a3b52d529d448618bd1a514a952b1931a9387b96
Zahid4960 <zahidcse4960@gmail.com>
2019-03-17 16:06:34 +0600

    My first program (Hello World) in c language

---
+++ Hello_World/helloWorld.c
@@ -0,0 +1,15 @@
+/* Header files */
+#include<stdio.h>
+
+/* Main function of the program where all the logices and conditions are executed*/
+int main(){
+
+/* Printf function print everything whatever between in this function*/
+printf("Hello World !!!");
+
+/* For newline*/
+printf("\n");
+
+/* Return 0 indicates the end of program*/
+return 0; 
+}
